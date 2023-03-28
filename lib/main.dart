7a3b28539fe3350cbf8c790e15b5f8421b46855d
tnvchdql0255 <tnvchdql0255@gmail.com>
2023-03-28 19:14:23 +0900

    Change mainPage UI

--- lib/main.dart
+++ lib/main.dart
@@ -25,48 +25,65 @@ class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
     var textStyle = const TextStyle(
-        color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600);
+        color: Colors.white, fontSize: 40, fontWeight: FontWeight.w600);
     return Scaffold(
       appBar: AppBar(title: const Text("Temp")),
       backgroundColor: Colors.lightBlue,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const Text(
-            "Game Title",
-            style: TextStyle(fontSize: 40, color: Colors.white60),
-          ),
-          TextButton(
-              onPressed: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const StageSelectionMenu()),
-                  ),
-              child: Text(
-                "게임 시작",
-                style: textStyle,
-              )),
-          TextButton(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Sensor IO",
+                  style: TextStyle(
+                      fontSize: 65,
+                      color: Colors.white60,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            TextButton(
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const StageSelectionMenu()),
+                    ),
+                child: Text(
+                  "게임 시작",
+                  style: textStyle,
+                )),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  "설정",
+                  style: textStyle,
+                )),
+            TextButton(
               onPressed: () {},
               child: Text(
-                "설정",
+                "기여자",
                 style: textStyle,
-              )),
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "기여자",
-              style: textStyle,
+              ),
             ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "나가기",
-              style: textStyle,
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                "나가기",
+                style: textStyle,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
