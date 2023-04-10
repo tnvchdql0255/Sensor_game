a5e489c0d264c202c4a433d8110ac70949d8df7a
ckqn06 <ckqn06@naver.com>
2023-04-11 01:51:23 +0900

    svg를 이용한 애니메이션 추가

--- lib/stage/stage_G_1.dart
+++ lib/stage/stage_G_1.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 import 'package:light/light.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sensor_game/common_ui/start.dart';
 import 'package:sensor_game/service/db_manager.dart';
 import 'package:sqflite/sqflite.dart';
@@ -17,6 +18,8 @@ class StageG1 extends StatefulWidget {
 class _StageG1State extends State<StageG1> {
   int _luxint = 0; //밝기의 값을 저장하는 _luxint 변수 선언
   int _bRGB = 255; //배경 화면의 RGB 값을 저장하는 _bRGB 변수 선언
+  String _asset =
+      'assets/images/stage_G_1_1.svg'; //졸린 사람의 svg를 저장하는 asset 변수 선언
   bool _isClear = false; //클리어 조건을 만족했는지를 체크하는 _isClear 변수 선언
 
   late Timer checkLightTimer; //1초마다 밝기 값이 낮은지를 체크하는 타이머를 위한 변수 선언
@@ -60,27 +63,28 @@ class _StageG1State extends State<StageG1> {
     }
 
     //1초마다 클리어 조건을 만족하는지 확인하는 타이머 생성
-    checkLightTimer = Timer.periodic(Duration(milliseconds: 1000), (timer) {
-      //만약 읽어들인 밝기 값이 15보다 작다면
-      if (_luxint < 15) {
+    checkLightTimer = Timer.periodic(Duration(milliseconds: 500), (timer) {
+      //만약 읽어들인 밝기 값이 15 이하라면
+      if (_luxint <= 15) {
         setState(() {
           lightList.add(_luxint); //읽어들인 밝기 값을 lightList에 추가
-          _bRGB -= 50; //배경 화면의 RGB 값을 50씩 감소
+          _bRGB -= 28; //배경 화면의 RGB 값을 28씩 감소
 
-          //만약 4초 동안 읽어들인 밝기 값이 15보다 작다면
-          if (lightList.length == 4 &&
-              lightList.every((element) => element < 15)) {
+          //만약 4초 동안 읽어들인 밝기 값이 15 이하라면
+          if (lightList.length > 2 && lightList.length <= 5) {
+            _asset = 'assets/images/stage_G_1_2.svg'; //svg를 변경
+          } else if (lightList.length > 5 && lightList.length <= 7) {
+            _asset = 'assets/images/stage_G_1_3.svg'; //svg를 변경
+          } else if (lightList.length >= 8 &&
+              lightList.every((element) => element <= 15)) {
             checkLightTimer.cancel(); //타이머를 종료
-            stopListening(); //밝기 값을 읽어들이는 것을 중지
-
-            lightList.clear(); //lightList를 비움
-            _bRGB = 0; //배경 화면의 RGB 값을 0으로 설정
-            _isClear = true; //클리어 조건을 만족했음을 알리는 isClear 변수를 true로 설정
+            _isClear = true; //클리어 조건을 만족했으므로 isClear 변수를 true로 설정
           }
         });
       } else {
         //그 외의 경우에는
         _bRGB = 255; //배경 화면의 RGB 값을 255로 설정
+        _asset = 'assets/images/stage_G_1_1.svg'; //svg를 초기화
         lightList.clear(); //lightList를 비움
       }
     });
@@ -103,7 +107,7 @@ class _StageG1State extends State<StageG1> {
 
     //1초마다 클리어 상태를 확인하는 타이머 생성
     checkClearTimer =
-        Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+        Timer.periodic(const Duration(milliseconds: 100), (timer) {
       clearStatus();
     });
   }
@@ -114,6 +118,11 @@ class _StageG1State extends State<StageG1> {
       if (_isClear == true) {
         //클리어 조건을 만족했다면
         checkClearTimer.cancel(); //타이머를 종료
+        stopListening(); //밝기 값을 읽어들이는 것을 중지
+
+        lightList.clear(); //lightList를 비움
+        _asset = 'assets/images/stage_G_1_4.svg'; //svg를 자는 사람으로 변경
+
         popUps.showClearedMessage(context).then((value) {
           //클리어 메시지를 출력
           if (value == 1) {
@@ -121,6 +130,7 @@ class _StageG1State extends State<StageG1> {
             initPlatformState(); //다시 시작할 시, 밝기 값을 읽어들이는 상태로 재설정
             setState(() {
               _bRGB = 255; //다시 시작할 시, 배경 화면의 RGB 값을 255로 재설정
+              _asset = 'assets/images/stage_G_1_1.svg'; //svg를 깨어있는 사람으로 변경
               _isClear = false; //다시 시작할 시, isClear 변수를 false로 재설정
             });
           }
@@ -152,23 +162,15 @@ class _StageG1State extends State<StageG1> {
           child: Center(
             //아래의 요소들을 가로로 가운데 정렬
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
                     //밝기 값을 출력하는 컨테이너
-                    child: Text('밝기 값: $_luxint, $lightList',
-                        style: TextStyle(fontSize: 30))),
+                    child:
+                        Text('밝기 값: $_luxint', style: TextStyle(fontSize: 20))),
                 Container(
                     //이미지를 출력하는 컨테이너
-                    width: 400,
-                    height: 400,
-                    decoration: BoxDecoration(
-                      //클리어 조건을 만족 시 자는 이미지를 출력하고, 그렇지 않으면 졸린 이미지를 출력
-                      image: DecorationImage(
-                          image: _isClear == false
-                              ? AssetImage('assets/images/insomnia.png')
-                              : AssetImage('assets/images/sleeping.png')),
-                    )),
+                    //클리어 조건을 만족 시 자는 이미지를 출력하고, 그렇지 않으면 졸린 이미지를 출력
+                    child: Expanded(child: SvgPicture.asset(_asset)))
               ],
             ),
           ),
